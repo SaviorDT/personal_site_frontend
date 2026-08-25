c75271bb4c5a41f4219ea83fffa8b5858f56b82a
deeelol <tony200404242@gmail.com>
2026-08-25 15:20:11 +0000

    Edit(url shortener): add "365d"

--- src/data/portfolio/UrlShortener/UrlShortener.jsx
+++ src/data/portfolio/UrlShortener/UrlShortener.jsx
@@ -9,6 +9,7 @@ const EXPIRE_OPTIONS = [
     { value: '1d', label: '1 天' },
     { value: '7d', label: '7 天' },
     { value: '30d', label: '30 天' },
+    { value: '365d', label: '1 年' },
 ];
 
 const UrlShortener = () => {
